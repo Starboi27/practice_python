0f38fdf9c4063086a622f13dd9afa6342fea6d6e
starboi27 <starboi27@users.noreply.github.com>
2026-03-19 14:08:46 +0900

    Upload problem solving code and ignore IDE settings

---
+++ step_5/11718.py
@@ -0,0 +1,14 @@
+while 1:
+    try:
+        a = input()
+    except EOFError:
+        break
+    print(a)
+
+"""
+while 1:
+    a = input()
+    if a == "":
+        break  
+    print(a)
+"""
---
+++ step_6/3003.py
@@ -0,0 +1,7 @@
+import numpy
+
+a = np.array(1, 1, 2, 2, 2, 8)
+b = [0, 1, 2, 2, 2, 7]
+
+result = a - b
+print(result)
